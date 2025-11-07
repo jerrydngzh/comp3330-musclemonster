0018c8761bf74fa383fe4e7bbaaa77d50da4a212
Ray Zhou <rayzhou4@gmail.com>
2025-11-07 16:50:38 +0800

    convert into android project

---
+++ app/src/main/java/hku/cs/comp3330_musclemonster/MainActivity.kt
@@ -0,0 +1,47 @@
+package hku.cs.comp3330_musclemonster
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import hku.cs.comp3330_musclemonster.ui.theme.Comp3330musclemonsterTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Comp3330musclemonsterTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Comp3330musclemonsterTheme {
+        Greeting("Android")
+    }
+}

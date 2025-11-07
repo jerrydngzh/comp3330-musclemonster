774510af0b813a494b476c3246b9d94c2cb62c5b
jerrydngzh <zrd1@sfu.ca>
2025-11-08 01:08:07 +0800

    add firebase firestore crud functionality (test this to make sure it works for you)

--- app/src/main/java/hku/cs/comp3330_musclemonster/MainActivity.kt
+++ app/src/main/java/hku/cs/comp3330_musclemonster/MainActivity.kt
@@ -1,9 +1,13 @@
 package hku.cs.comp3330_musclemonster
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.TextView
 import androidx.activity.ComponentActivity
 import androidx.activity.enableEdgeToEdge
+
+import com.google.firebase.Firebase
+import com.google.firebase.firestore.firestore
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,5 +18,28 @@ class MainActivity : ComponentActivity() {
         // Example of accessing a view
         val greetingText: TextView = findViewById(R.id.tvGreeting)
         greetingText.text = "Hello XML World!"
+
+        // init firestore
+        val db = Firebase.firestore
+
+        // TODO remove
+        // sanity check, making sure the firestore connection works
+//         Create a new user with a first and last name
+//        val user = hashMapOf(
+//            "first" to "Ada",
+//            "last" to "Lovelace",
+//            "born" to 1815,
+//        )
+//
+
+        // Add a new document with a generated ID
+//        db.collection("users")
+//            .add(user)
+//            .addOnSuccessListener { documentReference ->
+//                Log.d("test", "DocumentSnapshot added with ID: ${documentReference.id}")
+//            }
+//            .addOnFailureListener { e ->
+//                Log.w("test", "Error adding document", e)
+//            }
     }
 }

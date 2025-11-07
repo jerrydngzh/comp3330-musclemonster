de13ea33b3bf619e19ec341ccdbc1bea2230141e
Ray Zhou <rayzhou4@gmail.com>
2025-11-07 17:15:54 +0800

    dashboard activity + xml layout init

---
+++ app/src/main/java/hku/cs/comp3330_musclemonster/DashboardActivity.kt
@@ -0,0 +1,11 @@
+package hku.cs.comp3330_musclemonster
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class DashboardActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dashboard)
+    }
+}
